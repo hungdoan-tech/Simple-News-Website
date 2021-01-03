9c7a6e4f17a731e0a9f74e4195de46490b55db82
hungdoan-tech <hungdoan426@gmail.com>
2021-01-04 04:20:55 +0700

    Adding sitemesh lib and filter the whole app to use the new web interface

--- src/main/java/com/javacodingnews/admin/HomeController.java
+++ src/main/java/com/javacodingnews/admin/HomeController.java
@@ -1,6 +1,8 @@
 package com.javacodingnews.admin;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,8 +29,8 @@ public class HomeController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
